360b28140631cc84ed8861d6d63ae9a3007ff733
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2026-02-04 18:29:51 +0500

    logger

---
+++ core/logger.py
@@ -0,0 +1,37 @@
+import datetime
+from core.db import get_connection
+
+def log_request(query, answer, latency_ms, tokens_used, confidence, hallucination):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+    INSERT INTO requests(timestamp, query, answer, latency_ms, tokens_used, confidence, hallucination)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (
+        datetime.datetime.utcnow().isoformat(),
+        query,
+        answer,
+        latency_ms,
+        tokens_used,
+        confidence,
+        hallucination
+    ))
+    
+    conn.commit()
+    conn.close()
+    
+def log_error_message(error_message):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        INSERT INTO (timestamp, error_message)
+        VALUES(?, ?)
+    """, (
+        datetime.datetime.utcnow().isoformat(),
+        error_message
+    ))
+    
+    conn.commit()
+    conn.close()
