05e52398af26cc18319f92cd8247d4c771fea221
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2026-02-07 18:45:50 +0500

    import fixed

--- scripts/question_answer.py
+++ scripts/question_answer.py
@@ -5,5 +5,5 @@ embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 def get_best_chunk(question, chunks, index):
   question_embeddings = embedding_model.encode([question]).astype('float32')
 
-  _, I = index.search(question_embeddings, k = 5)
+  _, I = index.search(question_embeddings, k = 3)
   return chunks[I[0][0]]
